56b162630414902283521bffd54edb159e247164
Roberto <roberto_78@outlook.com.br>
2026-03-30 12:52:42 -0300

    fix: restaurar importação do React em ConferenciaView

--- cartorio_tcip_2/src/components/ConferenciaView.jsx
+++ cartorio_tcip_2/src/components/ConferenciaView.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { getApreensoes, updateApreensao, excluirApreensao } from "../services/api.js";
 
 // Formata peso com unidade inteligente
