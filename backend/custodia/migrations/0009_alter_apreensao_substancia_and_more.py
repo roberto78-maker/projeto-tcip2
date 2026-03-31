52508881098870c6740d3fd5215043ae076a7a6d
Roberto <roberto_78@outlook.com.br>
2026-03-31 13:24:20 -0300

    Fix: Formatação do Backend para liberar deploy da Grade Modular

--- backend/custodia/migrations/0009_alter_apreensao_substancia_and_more.py
+++ backend/custodia/migrations/0009_alter_apreensao_substancia_and_more.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('custodia', '0008_apreensao_natureza_apreensao_tem_apreensao_and_more'),
+        ("custodia", "0008_apreensao_natureza_apreensao_tem_apreensao_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='apreensao',
-            name='substancia',
+            model_name="apreensao",
+            name="substancia",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='historicalapreensao',
-            name='substancia',
+            model_name="historicalapreensao",
+            name="substancia",
             field=models.TextField(blank=True, null=True),
         ),
     ]
