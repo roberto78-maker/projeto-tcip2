f8c462f646637dc0b44b5a927b80f036f35c2bd4
Roberto <roberto_78@outlook.com.br>
2026-05-17 12:25:27 -0300

    feat: initialize Django project settings with Cloudinary, CORS, JWT, and Swagger configurations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+        conn_max_age=0 if not DEBUG else 600,
         ssl_require=os.environ.get("DATABASE_URL", "").startswith("postgres")
         and not DEBUG,
     )
