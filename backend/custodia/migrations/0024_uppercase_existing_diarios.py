d5056ba3bae7ce1a98b11cf6e38d71953d46b5d6
Roberto <roberto_78@outlook.com.br>
2026-08-10 18:17:46 -0300

    feat: migrate existing DiarioServico alteracoes fields to uppercase

--- backend/custodia/migrations/0024_uppercase_existing_diarios.py
+++ backend/custodia/migrations/0024_uppercase_existing_diarios.py
@@ -1,25 +1,29 @@
 from django.db import migrations
 
+
 def make_alteracoes_uppercase(apps, schema_editor):
-    DiarioServico = apps.get_model('custodia', 'DiarioServico')
+    DiarioServico = apps.get_model("custodia", "DiarioServico")
     for ds in DiarioServico.objects.all():
         if ds.alteracoes:
             ds.alteracoes = ds.alteracoes.upper()
-            ds.save(update_fields=['alteracoes'])
+            ds.save(update_fields=["alteracoes"])
 
     # Also update simple-history tracking tables to keep it consistent
-    HistoricalDiarioServico = apps.get_model('custodia', 'HistoricalDiarioServico')
+    HistoricalDiarioServico = apps.get_model("custodia", "HistoricalDiarioServico")
     for hds in HistoricalDiarioServico.objects.all():
         if hds.alteracoes:
             hds.alteracoes = hds.alteracoes.upper()
-            hds.save(update_fields=['alteracoes'])
+            hds.save(update_fields=["alteracoes"])
+
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('custodia', '0023_diarioservicoanexo'),
+        ("custodia", "0023_diarioservicoanexo"),
     ]
 
     operations = [
-        migrations.RunPython(make_alteracoes_uppercase, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(
+            make_alteracoes_uppercase, reverse_code=migrations.RunPython.noop
+        ),
     ]
